38382b29c80704acd82ee77368998b8f9edfb355
Im-Adriano <35747937+Im-Adriano@users.noreply.github.com>
2021-02-04 23:02:54 -0500

    Update getDoxybook2.sh

--- getDoxybook2.sh
+++ getDoxybook2.sh
@@ -1,7 +1,7 @@
-url=$(curl -s https://api.github.com/repos/matusnovak/doxybook2/releases/latest \ 
-  | grep browser_download_url \                                                                                
-  | grep linux \                                             
-  | cut -d '"' -f 4)                                
+url=$(curl -s https://api.github.com/repos/matusnovak/doxybook2/releases/latest \
+  | grep browser_download_url \
+  | grep linux \
+  | cut -d '"' -f 4)
 
 filename=${url##*/}          
 wget $url               
